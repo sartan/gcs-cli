de4d6aa1534254ee8f4d5ae563d85b8f81f5ccdb
Alex Sartan <asartan@pivotal.io>
2019-06-25 12:42:07 -0600

    Perform action on every sub directory, file in a path

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace DirectoryTraversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Environment.Exit(-1);
+            }
+
+            var startPath = Path.GetFullPath(NormalizeUserHomePath(args[0]));
+
+            if (!Directory.Exists(startPath))
+            {
+                return;
+            }
+
+            TraverseDirectory(startPath, GoatifyPath);
+        }
+
+        private static void TraverseDirectory(string path, Action<string> action)
+        {
+            action.Invoke(path);
+
+            foreach (var file in Directory.GetFiles(path))
+            {
+                action.Invoke(file);
+            }
+
+            foreach (var dir in Directory.GetDirectories(path))
+            {
+                TraverseDirectory(dir, action);
+            }
+        }
+
+        private static string NormalizeUserHomePath(string path)
+        {
+            return Regex.Replace(path, "^~", Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
+        }
+
+        private static void GoatifyPath(string str)
+        {
+            Console.WriteLine(str.Replace("/", "🐐/"));
+        }
+    }
+}
